5b79567b1b8fb325cf389150f09eab3786322f0a
Wan Saleh <wansaleh@gmail.com>
2022-01-11 11:00:55 +0800

    Updates.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,21 +16,21 @@
   export let names: Name[];
 </script>
 
-<div class="layout py-20 text-center">
+<div class="layout py-24 text-center">
   <h1 class="lg:text-8xl arabic relative mb-4 text-5xl font-semibold">
     أسماء ٱلحسنى
   </h1>
-  <h1 class="lg:text-7xl relative text-5xl font-semibold">
+  <h1 class="lg:text-6xl relative text-5xl font-semibold">
     <span>Asmaul Husna</span>
   </h1>
-  <h2 class="lg:text-4xl relative text-2xl italic">
+  <h2 class="lg:text-3xl relative text-2xl italic">
     <span>99 Nama Allah</span>
   </h2>
-  <h2 class="lg:text-4xl relative text-2xl">
+  <h2 class="lg:text-3xl relative text-2xl opacity-60">
     <span>99 Names of Allah</span>
   </h2>
 
-  <div class="mt-16">
+  <div class="mt-24">
     <div class="flex flex-row-reverse flex-wrap justify-center -m-2">
       {#each names as name (name.id)}
         <Card {name} />
